df8c0ccca69655e609cd2121f5fe3ac85fb8556b
Thomas <thomandoclases@gmail.com>
2025-08-14 11:42:54 -0300

    Implementación de la operación division

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,7 +3,6 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-
 public class Division
 {
     public static double Divide(int a, int b)
