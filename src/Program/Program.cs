f1d1b17c66f8c092a208a7164f97a0d97444639a
Thomas <thomandoclases@gmail.com>
2025-08-14 12:06:39 -0300

    Implementación de la operación substraccion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,10 +3,10 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-public class Division
+public class Substraction
 {
-    public static double Divide(int a, int b)
+    public static int Substract(int a, int b)
     {
-        return (double)a / b;
+        return a - b;
     }
 }
