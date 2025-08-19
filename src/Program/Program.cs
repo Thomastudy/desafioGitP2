9e6858df6ded3bf58ea5b6fd3d09711216757faa
nahueldlsl <nahueldelossanto@gmail.com>
2025-08-19 10:22:32 -0300

    Parentesis corregidos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -16,6 +16,8 @@ public class Division
     public static double Divide(int a, int b)
     {
         return (double)a / b;
+    }
+}
 
 // Esta clase implementa la operación suma
 public class Addition
