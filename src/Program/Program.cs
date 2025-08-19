23755db01350827ff839e5b569723dbdc26e0c60
nahueldlsl <nahueldelossanto@gmail.com>
2025-08-19 10:59:07 -0300

    Cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,36 +3,11 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-public class Substraction
-{
-    public static int Substract(int a, int b)
-    {
-        return a - b;
-    }
-}
-
-public class Division
-{
-    public static double Divide(int a, int b)
-    {
-        return (double)a / b;
-    }
-}
-
 // Esta clase implementa la operación suma
 public class Addition
 {
     public static int Add(int a, int b)
     {
-        return a + b;
-
-    }
-}
-// Esta clase implementa la operación multiplicación
-public class Multiplication
-{
-    public static int Multiply(int a, int b)
-    {
-        return a * b;
+        return 0; // Cambio incorrecto
     }
 }
